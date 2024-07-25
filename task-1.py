b31d521c7e5eb2f7b24ba3fdbf664ab1078c8076
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-25 16:24:17 +0300

    Refactor(PY) Code edits without fixing bugs or adding new features

--- task-1.py
+++ task-1.py
@@ -6,7 +6,6 @@ import time
 application_queue = queue.Queue()
 
 
-# Функція generate_request()
 def generate_request():
     # Створення нової заявки з унікальним номером
     request_id = random.randint(1, 1000)
@@ -15,7 +14,6 @@ def generate_request():
     application_queue.put(request_id)
 
 
-# Функція process_request()
 def process_request():
     # Якщо черга не порожня
     if not application_queue.empty():
@@ -31,9 +29,9 @@ def process_request():
 # Основний цикл програми
 try:
     while True:
-        # Виклик функції generate_request() для створення нових заявок
+        # Виклик функції для створення нових заявок
         generate_request()
-        # Виклик функції process_request() для обробки заявок
+        # Виклик функції для обробки заявок
         process_request()
         # Імітація затримки між операціями
         time.sleep(1)
